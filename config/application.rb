32272c9b114880e03eab6cc7ed8cf36efcb47e90
Gleds Strublic <gc.strublic@gmail.com>
2022-08-16 19:33:35 -0300

    Add config to factory_bot

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module UdemyQuizEnglishWord
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # config.factory_bot.definition_file_paths = ["custom/factories"]
   end
 end
