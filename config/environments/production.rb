719c51e3ab8d7bec9857df5ca032a939976adf3f
Gleds Strublic <gc.strublic@gmail.com>
2022-08-17 17:28:52 -0300

    Change project name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "udemy_quiz_english_word_production"
+  # config.active_job.queue_name_prefix = "quiz_pick_word_production"
 
   config.action_mailer.perform_caching = false
 
