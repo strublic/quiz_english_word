14d1e29af7aefaae1da533320dc18e9fbcc8c255
Gleds Strublic <gc.strublic@gmail.com>
2022-08-16 22:00:52 -0300

    Spec for new and create actions for word

--- spec/controllers/words_controller_spec.rb
+++ spec/controllers/words_controller_spec.rb
@@ -1,29 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe WordsController, type: :controller do
-    describe "GET index" do
-      before { get :index}
+  describe "GET index" do
+    before { get :index}
 
-      context "when some words present" do
-        let!(:word) {create(:word)}
+    context "when some words present" do
+      let!(:word) {create(:word)}
 
-        it "assigns @words" do
-          expect(assigns(:words)).to eq([word])
-        end
-
-        it "renders the index template" do
-          expect(response).to render_template("index")
-        end
+      it "assigns @words" do
+        expect(assigns(:words)).to eq([word])
       end
-      
-      context "when no words" do
-        it "assigns @words" do
-          expect(assigns(:words)).to eq([])
-        end
-         
-        it "renders the index template" do
-          expect(response).to render_template("index")
-        end
+
+      it "renders the index template" do
+        expect(response).to render_template("index")
       end
     end
+    
+    context "when no words" do
+      it "assigns @words" do
+        expect(assigns(:words)).to eq([])
+      end
+        
+      it "renders the index template" do
+        expect(response).to render_template("index")
+      end
+    end
+  end
+
+  describe "GET new" do
+    before { get :new}
+
+    let!(:word) {create(:word)}
+
+    it "assigns @words" do
+      expect(assigns(:word)).to be_a_new(Word)
+    end
+
+    it "renders the new template" do
+      expect(response).to render_template("new")
+    end
+  end
+
+  describe "POST create" do
+    subject { post :create, params: params }
+
+    context "valid params" do
+      let(:params) do 
+        { word: { value: "car", language: "english" } }
+      end
+
+      it 'creates new word' do
+        expect {subject}.to change(Word, :count).from(0).to(1)
+      end
+    end
+
+    context "invalid params" do
+      let(:params) do 
+        { word: { value: "car"} }
+      end
+
+      it 'does not create new word' do
+        expect {subject}.not_to change(Word, :count)
+      end
+    end
+  end
 end
