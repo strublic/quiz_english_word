acb2b0d9022a94ec4fc119279afb89bd67b120a7
Gleds Strublic <gc.strublic@gmail.com>
2022-08-18 22:48:42 -0300

    Use before_action

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,4 +1,6 @@
 class WordsController < ApplicationController
+    before_action :set_word, only: %i[show edit update destroy]
+
     def index
         @words = Word.all
     end
@@ -16,16 +18,11 @@ class WordsController < ApplicationController
         end
     end
 
-    def show
-        @word = Word.find(params[:id])
-    end
+    def show; end
 
-    def edit
-        @word = Word.find(params[:id])
-    end
+    def edit; end
 
     def update 
-        @word = Word.find(params[:id])
         if @word.update(word_params)
             redirect_to(word_path(@word))
         else
@@ -34,7 +31,6 @@ class WordsController < ApplicationController
     end
 
     def destroy
-        @word = Word.find(params[:id])
         @word.destroy
         redirect_to(words_path)
     end
@@ -43,5 +39,9 @@ class WordsController < ApplicationController
 
     def word_params
         params.require(:word).permit(:content, :language_id)
+    end
+
+    def set_word
+        @word = Word.find(params[:id])
     end
 end
